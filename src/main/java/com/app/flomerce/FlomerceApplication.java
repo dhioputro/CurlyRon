42fcc2f56016c02aa8a0483ba69052d8d25d4a2f
Dhio Putro <pramadhio.ariputro@astrapay.com>
2023-11-27 15:43:31 +0700

    second commit

---
+++ src/main/java/com/app/flomerce/FlomerceApplication.java
@@ -0,0 +1,13 @@
+package com.app.flomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlomerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlomerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/flomerce/FlomerceApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.app.flomerce;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FlomerceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
